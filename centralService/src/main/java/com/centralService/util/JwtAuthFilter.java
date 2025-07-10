137f79a23cd366551ea1b01e9b2e407e3e885f30
ubrioniks <ivan_semin_04@mail.ru>
2025-07-10 18:41:02 +0300

    update

--- centralService/src/main/java/com/centralService/util/JwtAuthFilter.java
+++ centralService/src/main/java/com/centralService/util/JwtAuthFilter.java
@@ -18,7 +18,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         String path = request.getRequestURI();
 
-        if (path.startsWith("/login") || path.startsWith("/register") || path.startsWith("/auth")) {
+
+        if (path.startsWith("/login") || path.startsWith("/register") || path.startsWith("/auth")
+                || path.startsWith("/css") || path.startsWith("/js") || path.startsWith("/images")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -26,10 +28,18 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         String token = request.getHeader("Authorization");
 
         if (token == null || !token.startsWith("Bearer ")) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("Требуется авторизация");
+            String accept = request.getHeader("Accept");
+
+            if (accept != null && accept.contains("text/html")) {
+                response.sendRedirect("/login");
+            } else {
+
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Unauthorized");
+            }
             return;
         }
+
 
         filterChain.doFilter(request, response);
     }
